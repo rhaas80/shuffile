324bcce327560f2c6583e76175dfc3cae1a919bd
Gregory A. Kosinovsky <kosinov@quartz386.llnl.gov>
2019-05-23 14:59:01 -0700

    dded test capability

--- test/test1.c
+++ test/test1.c
@@ -13,25 +13,61 @@
 
 #include "shuffile.h"
 
+#define TEST_PASS (0)
+#define TEST_FAIL (1)
+
 int main (int argc, char* argv[])
 {
+  int rc = TEST_PASS;
   MPI_Init(&argc, &argv);
 
-  int rank, ranks;
+  int rank, ranks, i;
+  int comm_restart_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &ranks);
 
   char buf[256];
-  sprintf(buf, "data from rank %d\n", rank);
+  void* buff[256];
+  sprintf(buf, "data from rank %d", rank);
 
   char filename[256];
-  sprintf(filename, "/tmp/moody20/testfile_%d.out", rank);
+  sprintf(filename, "/dev/shm/testfile_%d.out", rank);
+  printf("filename = /dev/shm/testfile_%d.out\n", rank);
+  errno = 0;
   int fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
   if (fd != -1) {
     write(fd, buf, strlen(buf));
+  printf("data = %s, rank=%d\n", buf,rank);
     close(fd);
   } else {
-    printf("Error opening file %s: %d %s\n", filename, errno, strerror(errno));
+    printf("Error opening write file %s: %d %s\n", filename, errno, strerror(errno));
+    rc = TEST_FAIL;
+  }
+  errno = 0;
+  int fdr = open(filename, O_RDONLY);
+  if (fdr != -1) {
+    int numBytes = read(fdr, buff, 100);
+    char* cbuff = (char*)buff;
+    cbuff[numBytes]  = '\0';
+    if(numBytes != strlen(buf)){
+      printf("wrote %d bytes to file, but read %d bytes from file\n", strlen(buf), numBytes);
+      rc = TEST_FAIL;
+    }
+    else{
+      for(i = 0; i < numBytes; i++){
+        if(buf[i] != cbuff[i]){
+          printf("%dth character writtten to file was %c, but %c was read from file\n",i, buf[i], cbuff[i]);
+          rc = TEST_FAIL;
+        }
+      }
+    }
+    printf("After first migrate, READ IN %d bytes\n", numBytes);
+    printf("After first migrate, READ IN %s\n", cbuff);
+    printf("data = %s, rank=%d\n", buf,rank);
+    close(fdr);
+  } else {
+    printf("Error opening read file %s: %d %s\n", filename, errno, strerror(errno));
+    rc = TEST_FAIL;
   }
 
   const char* filelist[1] = { filename };
@@ -42,13 +78,13 @@ int main (int argc, char* argv[])
   MPI_Comm comm_store = MPI_COMM_WORLD;
 
   /* associate files in filelist with calling process */
-  shuffile_create(comm_world, comm_store, 1, filelist, "/tmp/moody20/shuffle");
+  shuffile_create(comm_world, comm_store, 1, filelist, "/dev/shm/shuffle");
 
   /* migrate files back to owner process */
-  shuffile_migrate(comm_world, comm_store, "/tmp/moody20/shuffle");
-
+  shuffile_migrate(comm_world, comm_store, "/dev/shm/shuffle");
+     
   /* delete association information */
-  shuffile_remove(comm_world, comm_store, "/tmp/moody20/shuffle");
+  shuffile_remove(comm_world, comm_store, "/dev/shm/shuffle");
 
   /* assume running two procs per node in block distribution */
   MPI_Comm_split(comm_world, rank / 2, 0, &comm_store);
@@ -58,13 +94,44 @@ int main (int argc, char* argv[])
   MPI_Comm_split(comm_world, 0, ranks - rank, &comm_restart);
 
   /* associate files in filelist with calling process */
-  shuffile_create(comm_world, comm_store, 1, filelist, "/tmp/moody20/shuffle");
+  shuffile_create(comm_world, comm_store, 1, filelist, "/dev/shm/shuffle");
 
   /* migrate files back to owner process */
-  shuffile_migrate(comm_restart, comm_store, "/tmp/moody20/shuffle");
+  shuffile_migrate(comm_restart, comm_store, "/dev/shm/shuffle");
 
+  //sprintf(filename, "/dev/shm/testfile_%d.out", ranks-rank/2);
+  //sprintf(buf, "data from rank %d", ranks-rank/2);
+  MPI_Comm_rank(comm_restart, &comm_restart_rank);
+  sprintf(filename, "/dev/shm/testfile_%d.out", comm_restart_rank);
+  sprintf(buf, "data from rank %d", comm_restart_rank);
+  errno = 0;
+  fdr = open(filename, O_RDONLY);
+  if (fdr != -1) {
+    int numBytes = read(fdr, buff, 100);
+    char* cbuff = (char*)buff;
+    cbuff[numBytes]  = '\0';
+    if(numBytes != strlen(buf)){
+      printf("wrote %d bytes to file, but read %d bytes from file\n", strlen(buf), numBytes);
+      rc = TEST_FAIL;
+    }
+    else{
+      for(i = 0; i < numBytes; i++){
+        if(buf[i] != cbuff[i]){
+          printf("%dth character writtten to file was %c, but %c was read from file\n",i, buf[i], cbuff[i]);
+          rc = TEST_FAIL;
+        }
+      }
+    }
+    printf("After second migrate, READ IN %d bytes\n", numBytes);
+    printf("After second migrate, READ IN %s\n", cbuff);
+    printf("data = %s, rank=%d\n", buf,rank);
+    close(fdr);
+  } else {
+    printf("Error opening read file %s: %d %s\n", filename, errno, strerror(errno));
+    rc = TEST_FAIL;
+  }
   /* delete association information */
-  shuffile_remove(comm_world, comm_store, "/tmp/moody20/shuffle");
+  shuffile_remove(comm_world, comm_store, "/dev/shm/shuffle");
 
   MPI_Comm_free(&comm_store);
   MPI_Comm_free(&comm_restart);
@@ -73,5 +140,5 @@ int main (int argc, char* argv[])
 
   MPI_Finalize();
 
-  return 0;
+  return rc;
 }
