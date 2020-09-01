4a991a3fe8740b1055a09549f682a96f1065f3eb
Roland Haas <rhaas@illinois.edu>
2020-09-01 15:49:16 -0500

    test_config: add debug to tests

--- test/test_config.c
+++ test/test_config.c
@@ -11,7 +11,7 @@ main(int argc, char *argv[]) {
     int rc;
     kvtree* shuffile_config_values = kvtree_new();
 
-    /*int old_shuffile_debug = shuffile_debug;*/
+    int old_shuffile_debug = shuffile_debug;
     int old_shuffile_mpi_buf_size = shuffile_mpi_buf_size;
 
     MPI_Init(&argc, &argv);
@@ -23,12 +23,12 @@ main(int argc, char *argv[]) {
     }
 
     /* check shuffile configuration settings */
-    /*rc = kvtree_util_set_int(shuffile_config_values, SHUFFILE_KEY_CONFIG_DEBUG,
+    rc = kvtree_util_set_int(shuffile_config_values, SHUFFILE_KEY_CONFIG_DEBUG,
                              !old_shuffile_debug);
     if (rc != KVTREE_SUCCESS) {
         printf("kvtree_util_set_int failed (error %d)\n", rc);
         return rc;
-    }*/
+    }
     rc = kvtree_util_set_int(shuffile_config_values, SHUFFILE_KEY_CONFIG_MPI_BUF_SIZE,
                              old_shuffile_mpi_buf_size + 1);
     if (rc != KVTREE_SUCCESS) {
@@ -50,11 +50,11 @@ main(int argc, char *argv[]) {
         return rc;
     }
 
-    /*if (shuffile_debug != !old_shuffile_debug) {
+    if (shuffile_debug != !old_shuffile_debug) {
         printf("shuffile_config() failed to set %s: %d != %d\n",
                SHUFFILE_KEY_CONFIG_DEBUG, shuffile_debug, !old_shuffile_debug); 
         return EXIT_FAILURE;
-    }*/
+    }
 
     if (shuffile_mpi_buf_size != old_shuffile_mpi_buf_size + 1) {
         printf("shuffile_config() failed to set %s: %d != %d\n",
